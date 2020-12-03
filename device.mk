bf894a8349d6178b4e611e369389ea7f0e0fce08
Alexander Koskovich <zvnexus@outlook.com>
2020-12-03 10:26:15 +0000

    vayu: Disable the usage of ConfigStore.
     * As per commit https://github.com/LineageOS/android_hardware_interfaces/commit/11a113a67e87a8da9a9bf0a73883f2325de90934 ("Add disable_configstore").
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -137,6 +137,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Config Store
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # DeviceSettings
 PRODUCT_PACKAGES += \
     XiaomiParts
