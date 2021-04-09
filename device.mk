f3dc5358074370784cd14dac51145f04108701b7
Rafiester <rafi.haqqi@gmail.com>
2021-04-09 12:51:05 +0000

    vayu: Update HotwordEnrollment from Raphael
    * Taken from https://github.com/SOVIET-ANDROID/device_xiaomi_raphael/commit/1a61dc6d0c051f85a533dc4b03c4c4376742f20d#diff-4fe3cdd404ea0290bad96d3e1ccd92979d33ac63c4c18ad80e94d0452ce43391 & 2063bdc897fae379fe8eea9a9fd7cefaf5538e7a
    
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- device.mk
+++ device.mk
@@ -264,7 +264,9 @@ PRODUCT_PACKAGES += \
 
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/configs/permissions/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
+    $(LOCAL_PATH)/configs/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml
 
 # Init scripts
 PRODUCT_PACKAGES += \
