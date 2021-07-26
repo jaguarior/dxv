dca254f201702accab333ec69fa251427c21c05a
alanndz <alanndz@qmail.id>
2021-07-26 07:45:18 +0000

    vayu: nad: Build use Blur
    Signed-off-by: alanndz <alanndz@qmail.id>

--- nad_vayu.mk
+++ nad_vayu.mk
@@ -9,6 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 USE_GAPPS := true
+TARGET_USES_BLUR := true
 
 # Inherit some common stuff.
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
