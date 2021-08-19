4e19bc6706c3a555c11a7d286d55340b7237be0b
Genkzsz11 <Genkzsz11@gmail.com>
2021-08-19 23:05:40 +0700

    vayu: Remove gapps

--- nad_vayu.mk
+++ nad_vayu.mk
@@ -8,7 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-USE_GAPPS := true
 TARGET_USES_BLUR := true
 USE_PIXEL_CHARGING := true
 
