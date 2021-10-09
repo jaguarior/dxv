36c23d95d3b8814649a1be83163f24278cd100ba
Genkzsz11 <Genkzsz11@gmail.com>
2021-10-09 16:53:54 +0700

    vayu: Disable product boot jar for now

--- device.mk
+++ device.mk
@@ -253,7 +253,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
+#PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
 
 # Keylayout
@@ -403,7 +403,7 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
+#PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # USB
@@ -440,7 +440,7 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
+#PRODUCT_BOOT_JARS += \
     WfdCommon
 
 # Inherit the proprietary files
