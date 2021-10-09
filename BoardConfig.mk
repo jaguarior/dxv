a5b8bd5625cf1951318d818b9676d6acbda2f9a3
Genkzsz11 <Genkzsz11@gmail.com>
2021-10-09 16:51:38 +0700

    vayu: Selinux permissive for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,6 +88,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_vayu
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
